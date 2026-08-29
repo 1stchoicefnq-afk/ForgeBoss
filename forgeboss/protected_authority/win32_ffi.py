cf127876698f1134ecdd1915f7b66ef7188c2ce8
Jake | Building SiteBoss <1stchoicefnq@gmail.com>
2026-08-30 09:53:28 +1000

    tmp V6: add typed named-pipe state API

--- forgeboss/protected_authority/win32_ffi.py
+++ forgeboss/protected_authority/win32_ffi.py
@@ -22,6 +22,7 @@ def load_win32()->Win32API:
     ker.ReadFile.argtypes=[wintypes.HANDLE,wintypes.LPVOID,wintypes.DWORD,ctypes.POINTER(wintypes.DWORD),wintypes.LPVOID];ker.ReadFile.restype=wintypes.BOOL
     ker.WriteFile.argtypes=[wintypes.HANDLE,wintypes.LPCVOID,wintypes.DWORD,ctypes.POINTER(wintypes.DWORD),wintypes.LPVOID];ker.WriteFile.restype=wintypes.BOOL
     ker.DisconnectNamedPipe.argtypes=[wintypes.HANDLE];ker.DisconnectNamedPipe.restype=wintypes.BOOL
+    ker.SetNamedPipeHandleState.argtypes=[wintypes.HANDLE,ctypes.POINTER(wintypes.DWORD),ctypes.POINTER(wintypes.DWORD),ctypes.POINTER(wintypes.DWORD)];ker.SetNamedPipeHandleState.restype=wintypes.BOOL
     ker.WaitNamedPipeW.argtypes=[wintypes.LPCWSTR,wintypes.DWORD];ker.WaitNamedPipeW.restype=wintypes.BOOL
     ker.CreateFileW.argtypes=[wintypes.LPCWSTR,wintypes.DWORD,wintypes.DWORD,wintypes.LPVOID,wintypes.DWORD,wintypes.DWORD,wintypes.HANDLE];ker.CreateFileW.restype=wintypes.HANDLE
     adv.ConvertSidToStringSidW.argtypes=[wintypes.LPVOID,ctypes.POINTER(wintypes.LPWSTR)];adv.ConvertSidToStringSidW.restype=wintypes.BOOL
